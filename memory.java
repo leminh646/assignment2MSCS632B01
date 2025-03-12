fe0861c33f2a7651474116daeb45652b00606317
leminh646 <lebm646@gmail.com>
2025-03-12 14:30:12 -0700

    Create memory.java

---
+++ memory.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer[] numbers = {1, 2, 3, 4, 5};  // dynamically allocated array
+        System.out.println(numbers[0]);
+
+        // Java's garbage collector automatically frees memory when objects are no longer in use
+    }
+}
